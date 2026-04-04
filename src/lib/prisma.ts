3f83e096ae9d26063d76db599eebd577846eb2ec
victorestradaa <estradaa.vm@gmail.com>
2026-04-04 10:01:59 -0700

    Bypass AWS Amplify env limits for Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,13 @@
+// Inyectamos las variables de entorno de AWS si Amplify no las mapeó
+if (typeof process !== 'undefined') {
+  if (!process.env.DATABASE_URL) {
+    process.env.DATABASE_URL = 'postgresql://postgres.agdqreveheewmbujzlwe:*_N0viembre%3D%4012@aws-1-sa-east-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true&sslaccept=accept_invalid_certs';
+  }
+  if (!process.env.DIRECT_URL) {
+    process.env.DIRECT_URL = 'postgresql://postgres.agdqreveheewmbujzlwe:*_N0viembre%3D%4012@aws-1-sa-east-1.pooler.supabase.com:5432/postgres?sslmode=require&sslaccept=accept_invalid_certs';
+  }
+}
+
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
